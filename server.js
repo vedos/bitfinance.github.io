213d972e23ed17d7c497a08ac1697a924105371a
Vedad <sw_ws24@hotmail.com>
2019-11-02 21:44:58 +0100

    7 commit

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/eurobit'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/eurobit/index.html'));
 });
 
 // default Heroku PORT
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
